94b42a4f9d50772ce4b12d7a739b620755cf8f2a
Jun Li <jli@fec.gov>
2020-08-21 17:28:27 -0400

    Add logger info and DSL result sort by mur_no asc.

--- webservices/legal_docs/archived_murs.py
+++ webservices/legal_docs/archived_murs.py
@@ -1,25 +1,18 @@
+from sqlalchemy.sql import text
+from elasticsearch_dsl import Search, Q
+import logging
+import re
+from webservices.rest import db
+from webservices import utils
+from .reclassify_statutory_citation import reclassify_statutory_citation
+
 # import these 3 libraries to display the JSON format of object "mur"
 import json
 import datetime
 from json import JSONEncoder
 
-from sqlalchemy.sql import text
-
-
-from elasticsearch_dsl import Search, Q
-
-import logging
-import re
-# from collections import defaultdict
-
-# from webservices.env import env
-from webservices.rest import db
-# from webservices.utils import extend, create_eregs_link, get_elasticsearch_connection
-# from webservices.tasks.utils import get_bucket
-from webservices import utils
-from .reclassify_statutory_citation import reclassify_statutory_citation
-
 logger = logging.getLogger(__name__)
+LOCAL_DEBUG = False
 
 ALL_MURS = """
     SELECT DISTINCT
@@ -112,6 +105,17 @@ MUR_DOCUMENTS = """
     WHERE mur_id = %s
 """
 
+INSERT_DOCUMENT = """
+    INSERT INTO MUR_ARCH.DOCUMENTS(
+        document_id,
+        mur_no,
+        pdf_text,
+        mur_id,
+        length,
+        url
+    ) VALUES (:document_id, :mur_no, :pdf_text, :mur_id, :length, :url)
+"""
+
 
 # To display the open_date and close_date of JSON format inside object "mur"
 class DateTimeEncoder(JSONEncoder):
@@ -125,36 +129,38 @@ def load_archived_murs(mur_no=None):
     """
     Reads data for Archived MURs from a Postgres database (under schema:mur_arch),
     assembles a JSON document corresponding to the mur, and indexes this document
-    in Elasticsearch in the index `archived_murs` with a doc_type of `archived_murs`.
-    In addition, all documents attached to the case are uploaded to an
-    S3 bucket under the _directory_ `legal/<doc_type>/<id>/`.
+    in Elasticsearch in the index `archived_murs` with a doc_type of `murs`.
     """
     es = utils.get_elasticsearch_connection()
-    logger.info("Loading archived mur {0}(s)".format(mur_no))
+    logger.info("Loading archived mur{0}...".format(mur_no))
+    if LOCAL_DEBUG:
+        print("Loading archived mur{0}...".format(mur_no))
+
     mur_count = 0
-    print("load_archived_murs().......")
     for mur in get_murs(mur_no):
         if mur is not None:
             logger.info("Loading archived murs: {0}".format(mur["no"]))
-            print("Loading archived murs: {0}".format(mur["no"]))
+            if LOCAL_DEBUG:
+                print("Loading archived murs: {0}".format(mur["no"]))
+
             es.index("archived_murs", get_es_type(), mur, id=mur["doc_id"])
             mur_count += 1
-            logger.info("{0} archived mur(s) loaded".format(mur_count))
-            print("{0} archived mur(s) loaded".format(mur_count))
+
+            logger.info("{0} Archived Mur(s) loaded".format(mur_count))
+            if LOCAL_DEBUG:
+                print("{0} Archived Mur(s) loaded".format(mur_count))
         else:
             logger.info("Found an unpublished case - deleting archived mur: {0} from ES".format(mur["no"]))
             es.delete_by_query(index="docs_index", body={"query": {"term": {"no": mur["no"]}}},
                 doc_type=get_es_type())
             logger.info("Successfully deleted archived mur(s) {} from ES".format(mur["no"]))
 
-        # display the JSON format of object "mur"
-        print("mur_json_data =" + json.dumps(mur, indent=4, cls=DateTimeEncoder))
+        # ==for dubug use, display the JSON format of object "mur"
+        if LOCAL_DEBUG:
+            print("mur_json_data =" + json.dumps(mur, indent=4, cls=DateTimeEncoder))
 
 
 def get_murs(mur_no=None):
-    """
-    """
-    print("get_murs().......")
     if mur_no is None:
         with db.engine.connect() as conn:
             rs = conn.execute(ALL_MURS)
@@ -165,7 +171,6 @@ def get_murs(mur_no=None):
 
 
 def get_single_mur(mur_no):
-
     with db.engine.connect() as conn:
         rs = conn.execute(SINGLE_MUR, mur_no)
         row = rs.first()
@@ -183,7 +188,6 @@ def get_single_mur(mur_no):
             mur["complainants"] = get_complainants(mur_id)
             mur["respondents"] = get_respondents(mur_id)
             mur["citations"] = get_citations_arch_mur(mur_id)
-
             mur["subject"] = get_subjects(mur_id)
             mur["documents"] = get_documents(mur_id)
             return mur
@@ -212,6 +216,48 @@ def get_respondents(mur_id):
         for row in rs:
             respondents.append(row["name"])
     return respondents
+
+
+# citation sample data:
+# 1) us_code :
+#   ex1: original us_code: 2 U.S.C. 441a(a)(5)
+#        mapped us_code: 52 U.S.C. 30116(a)(5)
+#        url: https://www.govinfo.gov/link/uscode/52/30116
+
+#   ex2: 26 U.S.C. 9002(2)(A)
+#        url: https://www.govinfo.gov/link/uscode/26/9002
+
+# 2) regulation: 11 C.F.R. 9002.11(b)(3)
+#    url: /regulations/9002-11/CURRENT
+
+def get_citations_arch_mur(mur_id):
+    us_codes = []
+    regulations = []
+    with db.engine.connect() as conn:
+        rs = conn.execute(MUR_CITES, mur_id)
+        for row in rs:
+            if row["cite"]:
+                us_code_match = re.match(
+                    "(?P<title>[0-9]+) U\.S\.C\. (?P<section>[0-9a-z-]+)(?P<paragraphs>.*)", row["cite"])
+
+                regulation_match = re.match(
+                    "(?P<title>[0-9]+) C\.F\.R\. (?P<part>[0-9]+)(?:\.(?P<section>[0-9]+))?", row["cite"])
+
+                if us_code_match:
+                    us_code_title, us_code_section = reclassify_statutory_citation(
+                        us_code_match.group("title"), us_code_match.group("section"))
+                    citation_text = "%s U.S.C. %s%s" % (
+                        us_code_title, us_code_section, us_code_match.group("paragraphs"))
+                    url = "https://www.govinfo.gov/link/uscode/{0}/{1}".format(us_code_title, us_code_section)
+
+                    us_codes.append({"text": citation_text, "url": url})
+
+                elif regulation_match:
+                    url = utils.create_eregs_link(regulation_match.group("part"), regulation_match.group("section"))
+                    regulations.append({"text": row["cite"], "url": url})
+                else:
+                    raise Exception("Could not parse archived mur's citation.")
+        return {"us_code": us_codes, "regulations": regulations}
 
 
 def get_subjects(mur_id):
@@ -243,39 +289,6 @@ def get_subjects(mur_id):
     return subject_lv1
 
 
-# us_codes examples: 2 U.S.C. 431(1)(B) or 26 U.S.C. 9008(d)(1)
-# url:www.govinfo.gov/link/uscode/52/30101
-def get_citations_arch_mur(mur_id):
-    us_codes = []
-    regulations = []
-
-    with db.engine.connect() as conn:
-        rs = conn.execute(MUR_CITES, mur_id)
-        for row in rs:
-            if row["cite"]:
-                us_code_match = re.match(
-                    "(?P<title>[0-9]+) U\.S\.C\. (?P<section>[0-9a-z-]+)(?P<paragraphs>.*)", row["cite"])
-
-                regulation_match = re.match(
-                    "(?P<title>[0-9]+) C\.F\.R\. (?P<part>[0-9]+)(?:\.(?P<section>[0-9]+))?", row["cite"])
-
-                if us_code_match:
-                    us_code_title, us_code_section = reclassify_statutory_citation(
-                        us_code_match.group("title"), us_code_match.group("section"))
-                    citation_text = "%s U.S.C. %s%s" % (
-                        us_code_title, us_code_section, us_code_match.group("paragraphs"))
-                    url = "https://www.govinfo.gov/link/uscode/{0}/{1}".format(us_code_title, us_code_section)
-
-                    us_codes.append({"text": citation_text, "url": url})
-
-                elif regulation_match:
-                    url = utils.create_eregs_link(regulation_match.group("part"), regulation_match.group("section"))
-                    regulations.append({"text": row["cite"], "url": url})
-                else:
-                    raise Exception("Could not parse archived mur's citation.")
-        return {"us_code": us_codes, "regulations": regulations}
-
-
 def get_documents(mur_id):
     documents = []
     with db.engine.connect() as conn:
@@ -292,39 +305,33 @@ def get_documents(mur_id):
 
 def extract_pdf_text(mur_no=None):
     es = utils.get_elasticsearch_connection()
-
-    print ("extract_pdf_text()...")
     es_results = (
         Search()
         .using(es)
         .source(includes=["no", "documents.document_id", "documents.length", "documents.url", "documents.text"])
-        .extra(size=20)
+        .extra(size=6000)
         .index("archived_murs")
         .doc_type("murs")
+        .sort("no")
         .execute()
     )
     results = {"all_mur_docs": [hit.to_dict() for hit in es_results]}
-    print("results =" + json.dumps(results, indent=4, cls=DateTimeEncoder))
 
-    print("results.length=" + str(len(results['all_mur_docs'])))
+    logger.info("Get {0} archived mur(s) from elasticserch index: \"archived_murs\"".format(
+        str(len(results['all_mur_docs']))))
+    if LOCAL_DEBUG:
+        print("Get {0} archived mur(s) from elasticserch index: \"archived_murs\"".format(
+            str(len(results['all_mur_docs']))))
+        print("results = " + json.dumps(results, indent=4, cls=DateTimeEncoder))
 
-    INSERT_DOCUMENT = """
-        INSERT INTO MUR_ARCH.DOCUMENTS_TMP(
-            document_id,
-            mur_no,
-            pdf_text,
-            mur_id,
-            length,
-            url
-        ) VALUES (:document_id, :mur_no, :pdf_text, :mur_id, :length, :url)
-    """
-
-    insert_data_docs = []
     if results and results.get("all_mur_docs"):
         with db.engine.connect() as conn:
             for mur in results["all_mur_docs"]:
                 mur_no = mur["no"]
                 if mur.get("documents"):
+                    logger.info("Archived Mur{0} has documents.".format(mur_no))
+                    if LOCAL_DEBUG:
+                        print("Archived Mur{0} has documents.".format(mur_no))
                     for mur_doc in mur["documents"]:
                         if mur_doc:
                             insert_data_doc = ({
@@ -332,14 +339,45 @@ def extract_pdf_text(mur_no=None):
                                 "mur_no": mur_no,
                                 "pdf_text": mur_doc["text"],
                                 "mur_id": int(mur_no),
-                                "length": int(mur_doc["length"] or 0),
-                                "url": mur_doc["url"],
+                                "length": (int(mur_doc["length"]) or len(mur_doc["text"]) or None),
+                                "url": (mur_doc["url"] or "/files/legal/murs/{0}.pdf".format(mur_no)),
                             })
-
                             conn.execute(text(INSERT_DOCUMENT), **insert_data_doc)
-
-                            insert_data_docs.append(insert_data_doc)
-
-                # elif mur["text"]: TO DO List
-            print("insert_data_docs =" + json.dumps(insert_data_docs, indent=4, cls=DateTimeEncoder))
-
+                            logger.info("Archived Mur{0} has been inserted into table successfully".format(mur_no))
+                            if LOCAL_DEBUG:
+                                print("Archived Mur{0} has been inserted into table successfully".format(mur_no))
+                                print("Mur data = " + json.dumps(insert_data_doc, indent=4, cls=DateTimeEncoder))
+                elif mur.get("text"):
+                    logger.info("Archived Mur{0} has no documents, but has text.".format(mur_no))
+                    if LOCAL_DEBUG:
+                        print("Archived Mur{0} has no documents, but has text.".format(mur_no))
+                    insert_data_doc = ({
+                        "document_id": 1,
+                        "mur_no": mur_no,
+                        "pdf_text": mur["text"],
+                        "mur_id": int(mur_no),
+                        "length": (len(mur["text"]) or None),
+                        "url": "/files/legal/murs/{0}.pdf".format(mur_no),
+                    })
+                    conn.execute(text(INSERT_DOCUMENT), **insert_data_doc)
+                    logger.info("Archived Mur{0} has been inserted into table successfully".format(mur_no))
+                    if LOCAL_DEBUG:
+                        print("Archived Mur{0} has been inserted into table successfully".format(mur_no))
+                        print("Mur data = " + json.dumps(insert_data_doc, indent=4, cls=DateTimeEncoder))
+                else:
+                    logger.info("Archived Mur{0} has no documents.".format(mur_no))
+                    if LOCAL_DEBUG:
+                        print("Archived Mur{0} has no documents.".format(mur_no))
+                    insert_data_doc = ({
+                        "document_id": None,
+                        "mur_no": mur_no,
+                        "pdf_text": None,
+                        "mur_id": int(mur_no),
+                        "length": None,
+                        "url": "/files/legal/murs/{0}.pdf".format(mur_no),
+                    })
+                    conn.execute(text(INSERT_DOCUMENT), **insert_data_doc)
+                    logger.info("Archived Mur{0} has been inserted into table successfully".format(mur_no))
+                    if LOCAL_DEBUG:
+                        print("Archived Mur{0} has been inserted into table successfully".format(mur_no))
+                        print("Mur data = " + json.dumps(insert_data_doc, indent=4, cls=DateTimeEncoder))
